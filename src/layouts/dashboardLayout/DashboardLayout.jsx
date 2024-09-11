2c5b685933f15a1e107853d26418d00b8d870c73
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-11 16:37:34 +0530

    feat:implement dashboard page

--- src/layouts/dashboardLayout/DashboardLayout.jsx
+++ src/layouts/dashboardLayout/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet, useNavigate } from "react-router-dom"
 import "./dashboardLayout.css"
 import { useAuth } from "@clerk/clerk-react"
 import { useEffect } from "react"
+import ChatList from "../../components/chatList/ChatList"
 
 const DashboardLayout = () => {
   const { userId, isLoaded } = useAuth()
@@ -17,7 +18,9 @@ const DashboardLayout = () => {
 
   return (
     <div className="dashboardLayout">
-      <div className="menu">MENU</div>
+      <div className="menu">
+        <ChatList />
+      </div>
       <div className="content">
         <Outlet />
       </div>
