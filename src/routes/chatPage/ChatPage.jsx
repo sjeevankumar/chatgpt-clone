83a355c7c61d51ba879992e6cac715c4c668be83
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-12 16:43:27 +0530

    feat:add chatpage design

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,8 +1,52 @@
-import './chatPage.css'
+import NewPrompt from "../../components/newPrompt/NewPrompt"
+import "./chatPage.css"
 
 const ChatPage = () => {
   return (
-    <div className='chatPage'>ChatPage</div>
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from ai</div>
+          <div className="message user">
+            Test message from user Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci non labore voluptatum asperiores
+            accusantium maxime distinctio necessitatibus deleniti, ipsam vero.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">
+            Test message from user Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci non labore voluptatum asperiores
+            accusantium maxime distinctio necessitatibus deleniti, ipsam vero.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message user">
+            Test message from user Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci non labore voluptatum asperiores
+            accusantium maxime distinctio necessitatibus deleniti, ipsam vero.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message user">
+            Test message from user Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci non labore voluptatum asperiores
+            accusantium maxime distinctio necessitatibus deleniti, ipsam vero.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message user">
+            Test message from user Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Adipisci non labore voluptatum asperiores
+            accusantium maxime distinctio necessitatibus deleniti, ipsam vero.
+          </div>
+          <div className="message">Test message from ai</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt />
+        </div>
+      </div>
+    </div>
   )
 }
 
