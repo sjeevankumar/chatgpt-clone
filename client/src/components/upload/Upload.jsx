d60ec2bffeb9178512ab2cd9f315e5ab044f69f2
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-15 16:50:22 +0530

    feat:integrate gemini ai api

--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -39,8 +39,22 @@ const Upload = ({ setImg }) => {
   }
 
   const onUploadStart = (evt) => {
-    console.log(evt)
-    setImg((prev) => ({ ...prev, isLoading: true }))
+    const file = evt.target.files[0]
+
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      setImg((prev) => ({
+        ...prev,
+        isLoading: true,
+        aiData: {
+          inlineData: {
+            data: reader.result.split(",")[1],
+            mimeType: file.type,
+          },
+        },
+      }))
+    }
+    reader.readAsDataURL(file)
   }
 
   return (
