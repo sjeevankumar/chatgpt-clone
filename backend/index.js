5726da07921cf15e2ae5a65b3b2a3fa714af5772
sjeevankumar <sjeevankumar89@gmail.com>
2024-09-16 13:59:19 +0530

    feat:implement add a new chat backend api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import ImageKit from "imagekit"
 import cors from "cors"
+import mongoose from "mongoose"
+import Chat from "./models/chat.js"
+import UserChats from "./models/userChats.js"
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -10,6 +13,17 @@ app.use(
     origin: process.env.CLIENT_URL,
   })
 )
+
+app.use(express.json())
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO)
+    console.log("connected to mongodb...")
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -22,6 +36,55 @@ app.get("/api/upload", (req, res) => {
   res.send(result)
 })
 
-app.listen(port, () => {
+app.post("/api/chats", async (req, res) => {
+  const { userId, text } = req.body
+
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId: userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    })
+
+    const savedChat = await newChat.save()
+
+    // CHECK IF THE USERCHATS EXISTS
+    const userChats = await UserChats.find({ userId: userId })
+
+    //IF DOESNT EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId: userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+          },
+        ],
+      })
+      await newUserChats.save()
+    } else {
+      // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+      await UserChats.updateOne(
+        { userId: userId },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(),
+            },
+          },
+        }
+      )
+    }
+    res.status(201).send(newChat._id)
+  } catch (error) {
+    console.log(err)
+    res.status(500).send("Error creating chat!")
+  }
+})
+
+app.listen(port, async () => {
+  await connect()
   console.log(`Server running on port ${port}`)
 })
